3add081a2872af07350dd57e1b4f968cca16d917
carly <carly@noukod.com>
2019-01-24 20:07:36 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def show
+    set_student
+  end
+
+  def create
+    @student = Student.create student_params(:first_name, :last_name)
+		redirect_to student_path(@student)
+  end
+
+  def edit
+    set_student
+  end
+
+  def update
+    set_student
+    @student.update student_params(:first_name, :last_name)
+    redirect_to student_path
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+end
